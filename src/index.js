4c448ab61b1324ff3bb0f648a082e1a6c24391a2
CJ-Dashboard <ldh99914488@gmail.com>
2026-05-13 11:11:53 +0900

    Vercel Analytics 추가

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { inject } from '@vercel/analytics';
 import App from './App';  
   
 inject();  
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
+const root = ReactDOM.createRoot(document.getElementById('root'));  
+root.render(  
+  <React.StrictMode>  
+    <App />  
+  </React.StrictMode>  
+);  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
