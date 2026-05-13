3c983f2673b0b90f74699599c3fb181ad8ea7fd3
CJ-Dashboard <ldh99914488@gmail.com>
2026-05-13 10:53:52 +0900

    Vercel Analytics 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react';  
+import ReactDOM from 'react-dom/client';  
+import { inject } from '@vercel/analytics';  
+import App from './App';  
+  
+inject();  
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
